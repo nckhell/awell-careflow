b91a6f16c545b4763eefd1c4bf2b0ab16935acb4
Nick Hellemans <nickhellemans@Nicks-MacBook-Pro-2.local>
2023-11-29 18:28:56 +0100

    chore(): remove console.log

--- tests/steps.test.js
+++ tests/steps.test.js
@@ -3,7 +3,6 @@ const steps = require('../steps/steps.json');
 describe('Steps', () => {
   it('Step should have "Message" as a title', () => {
     const messageStep = steps.find(d => d.step.definition_id === 'USlw4pE-bXT5').step
-    console.log(messageStep)
 
     expect(messageStep.title).toBe('Message');
   });
